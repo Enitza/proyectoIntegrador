092da6919d538dc0c93c4a969768b305b16ef0d8
Enitza <enitza_0602@hotmail.com>
2023-09-20 22:04:53 -0500

    Ingreso de lectura de teclas

--- proyectoInterMain.py
+++ proyectoInterMain.py
@@ -1,2 +1,13 @@
+import readchar
+
+#Parte 1|
 nombre = input ("Ingresa tu Nombre: ")
-print ("Bienvenido," , nombre)
+print ("Bienvenido,", nombre)
+
+#Parte 2, se utiliza la tecla w como "up" debido a que no lee correctamente la flecha arriba del teclado
+
+while True:
+    teclas = readchar.readkey()
+    print ("Tecla Leida:", teclas)
+    if teclas == 'w' or teclas == 'W':
+        break
