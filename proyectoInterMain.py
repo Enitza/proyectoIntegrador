fe2f9f88087cd33e6723ab39844da670023f55f2
Enitza <enitza_0602@hotmail.com>
2023-09-27 20:37:00 -0500

    Ingreso lectura de tecla n que aumenta de 0 a 50

--- proyectoInterMain.py
+++ proyectoInterMain.py
@@ -1,4 +1,5 @@
-import readchar
+import readchar, os
+
 
 #Parte 1
 nombre = input ("Ingresa tu Nombre: ")
@@ -11,3 +12,18 @@ while True:
     print ("Tecla Leida:", teclas)
     if teclas == 'w' or teclas == 'W':
         break
+
+#Parte 3, cada vez que se oprime la tecla n se imprime un numero desde 0 y aumenta hasta 50
+
+def terminal(numero): 
+    os.system('cls' if os.name=='nt' else 'clear')
+    print(numero)
+
+n = 0
+
+while n<=50:
+    print("Ingrese Tecla")
+    leer = readchar.readkey()
+    if leer=='n' or leer=='N':
+        terminal(n)
+        n += 1
